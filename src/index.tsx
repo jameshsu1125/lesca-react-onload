07604ddf2c26324bad26d724dadc9322f1c76445
James Hsu <jameshsu1125@gmail.com>
2025-08-06 15:28:02 +0800

    feat: 新增 lesca-image-onload 依賴，更新 OnloadProvider 組件以支援圖片加載，並調整根組件以顯示加載進度

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,28 @@
-const App = () => {
-  return <div>app</div>;
+import ImageOnload from 'lesca-image-onload';
+import { Children, cloneElement, useEffect, useRef } from 'react';
+import { ImageOnloadResult, OnLoaderProps } from './type';
+
+const OnloadProvider = ({
+  children,
+  hideBeforeLoaded = true,
+  onStep = () => {},
+  onload = () => {},
+}: OnLoaderProps) => {
+  const ref = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      new ImageOnload()
+        .load(ref.current, { hideBeforeLoaded, onUpdate: (e: any) => onStep?.(e) })
+        .then((e: any) => {
+          onload?.(e as ImageOnloadResult);
+        });
+    }
+  }, []);
+  return Children.map(children, (child) => {
+    if (!child || typeof child !== 'object' || !('props' in child)) return child;
+    return cloneElement(child as React.ReactElement, { ...(child as any).props, ref });
+  });
 };
 
-export default App;
+export default OnloadProvider;
--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,55 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '.';
+import OnloadProvider from '.';
+
+const ComponentNeedToPreload = () => {
+  const [percent, setPercent] = useState(0);
+  const [info, setInfo] = useState('');
+  return (
+    <div className='my-4'>
+      <OnloadProvider
+        hideBeforeLoaded={false}
+        onStep={(e: any) => {
+          const { total, loaded, url } = e;
+          setPercent(Math.floor((loaded / total) * 100));
+          setInfo(`target: ${url} was loaded.`);
+        }}
+        onload={(e: any) => {
+          const { total, loaded } = e;
+          setPercent(Math.floor((loaded / total) * 100) || 100);
+          setInfo(`all target was loaded`);
+        }}
+      >
+        <div className='flex w-full flex-row flex-wrap items-center justify-center'>
+          {Array.from(Array(16).keys()).map((e) => (
+            <img
+              key={e}
+              src={`https://picsum.photos/id/${10 + e * 3 + e}/350/300`}
+              className='m-2'
+            />
+          ))}
+        </div>
+      </OnloadProvider>
+      <div className='w-full'>
+        <progress className='progress progress-primary w-full' value={percent} max='100' />
+        <div className='text-primary w-full'>{info}</div>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [currentTarget, setCurrentTarget] = useState(false);
+  return (
+    <div className='flex w-full justify-center'>
+      <div className='w-full max-w-4xl p-20'>
+        {currentTarget && <ComponentNeedToPreload />}
+        <button onClick={() => setCurrentTarget(true)} className='btn'>
+          load component
+        </button>
+      </div>
+    </div>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('app')!).render(<App />);
